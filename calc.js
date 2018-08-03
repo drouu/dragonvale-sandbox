797ef260c58d41b80d9b86d3fa2aee41a01a69e4
drow <drow@bin.sh>
2018-08-03 00:07:24 -0500

    attribution

--- calc.js
+++ calc.js
@@ -1,7 +1,7 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // dvbox/calc.js
 //
-// written and released to the public domain by drow <drow@bin.sh>
+// by drow <drow@bin.sh>
 // http://creativecommons.org/publicdomain/zero/1.0/
 //
 // requires Prototype
