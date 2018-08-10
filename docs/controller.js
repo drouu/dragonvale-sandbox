61e01dd7e4d24ab19e6ed5dae1e72128f9430790
drow <drow@bin.sh>
2018-08-09 19:58:59 -0500

    docs

--- docs/controller.js
+++ docs/controller.js
@@ -17,6 +17,32 @@
       $('d1').insert(Builder.node('option',attr,name));
       $('d2').insert(Builder.node('option',attr,name));
     });
+    $('d1').observe('change',update_results);
+    $('d2').observe('change',update_results);
+    $('beb').observe('change',update_results);
+
+    update_results();
+  }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// update results
+
+  function update_results () {
+    var d1 = $('d1').getValue();
+    var d2 = $('d2').getValue();
+    var beb = $('beb').getValue();
+    var list = breed_calc(d1,d2,beb);
+
+    if (list) {
+      $('results').update('');
+
+      list.sort().each(function (key) {
+        var name = dragons[key]['name'];
+        $('results').insert(Builder.node('tr',Builder.node('td',name));
+      });
+    } else {
+      $('results').update('No dragons');
+    }
   }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
