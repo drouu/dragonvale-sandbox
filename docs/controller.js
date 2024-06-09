18fc98f813ab832c36020b64d788b7de1af8ad67
drow <drow@bin.sh>
2024-06-09 01:46:49 -0500

    update docs/controller

--- docs/controller.js
+++ docs/controller.js
@@ -5,29 +5,25 @@
 // http://unlicense.org/
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// requires Prototype
-// http://prototypejs.org/
-//
-// requires script.aculo.us/Builder
-// http://script.aculo.us/
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // init sandbox
 
   function init_sandbox () {
+    let d1 = document.getElementById('d1');
+    let d2 = document.getElementById('d2');
+    let beb = document.getElementById('beb');
+
     Object.keys(dragons).sort().forEach(key => {
-      let attr = { 'value': key };
       let name = dragons[key]['name'];
 
-      $('d1').insert(Builder.node('option',attr,name));
-      $('d2').insert(Builder.node('option',attr,name));
+      d1.insert(build_option(key,name));
+      d2insert(Builder.node(key,name));
     });
-    $('d1').setValue('Fire_Dragon');
-    $('d2').setValue('Plant_Dragon');
+    d1.value = 'Fire_Dragon';
+    d2.value = 'Plant_Dragon';
 
-    $('d1').observe('change',update_results);
-    $('d2').observe('change',update_results);
-    $('beb').observe('change',update_results);
+    d1.addEventListener('change',update_results);
+    d2.addEventListener('change',update_results);
+    beb.addEventListener('change',update_results);
 
     update_results();
   }
@@ -36,29 +32,54 @@
 // update results
 
   function update_results () {
-    let d1 = $('d1').getValue();
-    let d2 = $('d2').getValue();
-    let beb = $('beb').getValue();
+    let d1 = document.getElementById('d1').value;
+    let d2 = document.getElementById('d2').value;
+    let beb = document.getElementById('beb').value;
     let list = breed_calc(d1,d2,beb);
+    let results = document.getElementById('results');
 
     if (list) {
-      $('results').update('');
+      results.replaceChildren();
 
       list.sort().forEach(key => {
-        $('results').insert(Builder.node('tr',[
-          Builder.node('td',dragons[key]['name']),
-          Builder.node('td',dragons[key]['dhms']),
-          Builder.node('td',dragons[key]['habitats'].join(', ')),
-        ]));
+        let dragon = dragons[key];
+        let tr = document.createElement('tr');
+
+        tr.appendChild(build_node('td',dragon['name'));
+        tr.appendChild(build_node('td',dragon['dhms'));
+        tr.appendChild(build_node('td',dragon['habitats').join(', '));
+
+        results.appendChild(tr);
       });
     } else {
-      $('results').update('No dragons');
+      let tr = document.createElement('tr');
+
+      tr.appendChild(build_node('td','No dragons'));
+      results.appendChild(tr);
     }
+  }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// build node
+
+  function build_node (type, text) {
+    let node = document.createElement(type); 
+    node.appendChild(document.createTextNode(text));
+
+    return node;
+  }
+  function build_option (value, text) {
+    let node = build_node('option',text);
+
+    node.setAttribute('value', value);
+    return node;
   }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // showtime
 
-  document.observe('dom:loaded',init_sandbox);
+  document.addEventListener('DOMContentLoaded', (event) => {
+    init_sandbox();
+  });
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
